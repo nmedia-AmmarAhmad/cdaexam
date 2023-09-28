1b54fab19a8996c7cfd9fdf6ea47aad99707f36f
EC2 Default User <ec2-user@ip-172-31-15-234.ec2.internal>
2023-09-28 01:21:26 +0000

    Initial commit

---
+++ test/final-exam.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FinalExam from '../lib/final-exam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/final-exam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FinalExam.FinalExamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
